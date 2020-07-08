ec48b3458459d140e94a4219c422c3963df8011f
alberto mineo <teloconfesso@gmail.com>
2020-07-08 05:31:39 +0200

    Add get request of Structures

--- src/components/StructuresList.js
+++ src/components/StructuresList.js
@@ -4,106 +4,34 @@ import Constants from 'expo-constants';
 import colors from "../style/colors/index";
 import BookingButton from "../components/buttons/bookingButton";
 import { useNavigation } from '@react-navigation/native';
-
-
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'Struttura 1',
-    price: 34,
-    place: 'Palermo',
-    kitchen: false,
-    fullBoard: true,
-    airConditioner:true,
-    wifi:false,
-    parking:false
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Struttura 2',
-    price: 45,
-    place: 'Catania',
-    kitchen: true,
-    fullBoard: false,
-    airConditioner:false,
-    wifi:true,
-    parking:true
-
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'struttura 3',
-    price: 120,
-    place: 'Firenze',
-    kitchen: true,
-    fullBoard: true,
-    airConditioner:true,
-    wifi:false,
-    parking:true
-
-  },
-  {
-    id: '58694a0f-s3da1-471f-bd96-145571e29d72',
-    title: 'Struttura 4',
-    price: 68,
-    place: 'Roma',
-    kitchen: true,
-    fullBoard: false,
-    airConditioner:true,
-    wifi:true,
-    parking:false
-
-  },
-  {
-    id: '58694a0f-3dda1-471f-bd96-145571e29d72',
-    title: 'Struttura 5',
-    price: 50,
-    place: 'Palermo',
-    kitchen: false,
-    fullBoard: true,
-    airConditioner:false,
-    wifi:false,
-    parking:true
-  },
-  
-];
-
-
-
-
-
-function Item({ title,price,id,place,kitchen,fullBoard,airConditioner,wifi,parking }) {
-  
-  const navigation = useNavigation();
-
-  return (
-    
-    <View style={styles.item}>
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.place}>{place}</Text>
-
-      <Text style={styles.services}>Servizi inclusi :</Text>
-      <BookingButton text={parseInt(price)+'€ a Notte'} 
-        onPress={()=>navigation.navigate('Structure',{
-            /* parametri da passare alla schermata successiva */
-            itemTitle: title,
-            itemPrice: price,
-            itemID: id,
-            ItemPlace: place,
-            itemKitchen: kitchen,
-            itemFullBoard: fullBoard,
-            itemAirConditioner: airConditioner,
-            itemWifi: wifi,
-            itemParking: parking,
-        })}></BookingButton>
-    </View>
-  );
-}
+import axios from "axios";
 
 export default class StructuresList extends Component {
    constructor(props){
      super(props);
+     this.state = {
+      data: []
+      }
    }
+
+   componentDidMount = () => {
+    const url = `http://localhost:3055/structures`;
+    axios(url, {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json',
+        },
+        params: {
+         code: 'structures'
+        },
+      })
+      .then(res => {
+        console.log(res.data);
+        const structures = res.data;
+        this.setState({data: structures});
+        console.log(this.state.data);
+    })
+  }
 
   dataCityFilter(DATA){
     var newData=[]
@@ -162,26 +90,20 @@ export default class StructuresList extends Component {
       
       <View style={styles.container}>
         <FlatList
-          data={this.dataServicesFilter(this.dataCityFilter(this.dataPriceFilter(DATA)))}
-          renderItem={(
-            { item }) => <Item 
-                            title={item.title} 
-                            price={item.price} 
-                            id={item.id} 
-                            kitchen={item.kitchen}
-                            fullBoard={item.fullBoard}
-                            airConditioner={item.airConditioner}
-                            wifi={item.wifi}
-                            place={item.place} 
-                            parking={item.parking} 
-                            price={item.price}
-                          />}
-          keyExtractor={item => item.id}
+          data={this.dataServicesFilter(this.dataCityFilter(this.dataPriceFilter(this.state.data)))}
+          keyExtractor = {(item, index) => index.toString()}
+          renderItem = {({item}) =>
+          <View style={styles.item}>
+            <Text>{item.title}</Text>
+            <Text>{item.place}</Text>
+            <Text style={styles.services}>Servizi inclusi :</Text>
+            <BookingButton text={parseInt(item.price)+'€ a Notte'}></BookingButton>
+          </View>}
           contentContainerStyle={{paddingTop:40}}
           ListFooterComponent={
-            <View styles={styles.footer}>
-              <Text styles={styles.footerText}>*la disponibilità può variare in base alle date di permanenza</Text>
-            </View>
+          <View styles={styles.footer}>
+            <Text styles={styles.footerText}>*la disponibilità può variare in base alle date di permanenza</Text>
+          </View>
           }
         />
       </View>
