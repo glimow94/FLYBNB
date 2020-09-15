71b99284f297cdc049ddcb741829b809c42e46d6
giuseppe <gdipiazza994@gmail.com>
2020-09-15 20:02:36 +0200

    fix

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -94,7 +94,7 @@ class Home extends Component{
             {Platform.OS === 'web'? 
               <View style={{alignContent:'center',alignItems:'center'}}>
                 { this.state.token != null ?
-                  <TouchableOpacity onPress={()=>navigation.navigate('Accedi')} style={styles.accessButton}> 
+                  <TouchableOpacity onPress={()=>navigation.jumpTo('MainTab')} style={styles.accessButton}> 
                     <Icon
                       size={40}
                       style={styles.icon}
